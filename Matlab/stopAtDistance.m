8cebf33fd8c9c24743ae5ad792678c13a33ae29f
Sieger Falkena <s.falkena@forze-delft.nl>
2018-04-29 18:18:37 +0200

    Implemented a new version of stopAtDistance, which accounts for speed as well (needs to be tuned)

--- Matlab/stopAtDistance.m
+++ Matlab/stopAtDistance.m
@@ -1,22 +1,27 @@
-count = 0;
+global d_l_log d_r_log stop_distance
+
 EPOCommunications('transmit','M165');
-aantal_metingen=0;
-while (count < 1)
-    d=zeros(1,2);
-    [d(1), d(2)] = checkDistance()
-    minimum = min(d);
-    if(minimum < 200)
-        count = count+1;
+
+%Intitialize all variables
+d=zeros(1,2);
+d = [d_l_log(end), d_r_log(end)];
+maximum=max(d(1),d(2));
+Speed=diff(d_l_log(end))/diff(t(end)); %speed: 0-5.56 m/s
+brake_distance = stop_distance;
+
+while(maximum > brake_distance)
+    d = [d_l_log(end), d_r_log(end)];
+    maximum=max(d(1),d(2));
+    if(length(d_l_log) < 3)
+        Speed = diff(d_l_log(end))/diff(t(end));
     else
-        count = 0;
+        %take mean of derivative of the last 3 elements
+        Speed = mean(diff(d_l_log(end-3:end)))/mean(diff(t(end-3:end)));
     end
-    aantal_metingen = aantal_metingen + 1;
+    brake_distance = (stop_distance - Speed*5);
 end
 
+%braking according to speed
 EPOCommunications('transmit','M135');
-pause(0.5)
-EPOCommunications('transmit','M150');
-pause(1)
-EPOCommunications('transmit','M135');
-pause(2)
+pause(Speed/4);
 EPOCommunications('transmit','M150');
