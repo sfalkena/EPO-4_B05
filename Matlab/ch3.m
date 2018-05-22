9916d3571e7d557d906a4e50034d93d875e3915d
Sumeet Sharma <sumeet_sharma99@hotmail.com>
2018-05-22 10:30:18 +0200

    Ch3 including filtering of channel (without crashing)

--- Matlab/ch3.m
+++ Matlab/ch3.m
@@ -1,10 +1,15 @@
 function [ hfil ] = ch3(y,x)
-eps = 0.001;                      %threshold
+eps = 5;                      %threshold
 Ny = length(y);
 Nx = length(x);
 Y = fft(y);
 X = fft([x zeros(1,Ny-Nx)]);   %zero padding
 H = Y./X;
+
+indices = find(abs(H)<eps);
+figure
+plot(abs(H));
+H(indices) = 0;
 hfil = ifft(H);
 figure
 plot(hfil);
