fdb67e29cf459468e4c2ae3e7a36fd0ac9d770e0
Sander Delfos <sdelfos94@gmail.com>
2018-05-25 22:36:54 +0200

    It will always find a path now, now matter what starting position or orientation. Need algorithm to decide whether to turn right or left now.

--- Matlab/Final/trajectory.m
+++ Matlab/Final/trajectory.m
@@ -1,7 +1,7 @@
 clear all
-KITTStartX = 50;
-KITTStartY = 50;
-KITTStartOrientation = 0;
+KITTStartX = 300;
+KITTStartY = 300;
+KITTStartOrientation = 90;
 KITTStartOrientation = deg2rad(KITTStartOrientation);
 
 figure
@@ -21,26 +21,27 @@ scatter(KITTStartX,KITTStartY,'g')
 
 %plot turning radius
 r = 87.5;
-for i=1:100
-    KITTx(i) = r*cos(-0.1*(i-1)+KITTStartOrientation+pi)+KITTStartX +r*cos(KITTStartOrientation);
-    KITTy(i) = r*sin(-0.1*(i-1)+KITTStartOrientation+pi)+KITTStartY +r*sin(KITTStartOrientation);
+e = 0.001;
+for i=1:999999
+    KITTx(i) = r*cos(-e*(i-1)+KITTStartOrientation+pi)+KITTStartX +r*cos(KITTStartOrientation);
+    KITTy(i) = r*sin(-e*(i-1)+KITTStartOrientation+pi)+KITTStartY +r*sin(KITTStartOrientation);
     if (i>1)
-        KITTtangent = (KITTy(i)-KITTy(i-1))/(KITTx(i)-KITTx(i-1));
+        KITTdeltaX = (KITTx(i)-KITTx(i-1));
+        KITTdeltaY = (KITTy(i)-KITTy(i-1));
+        KITTtangent = KITTdeltaY/KITTdeltaX;
         toTargetX = (Px(1)-KITTx(i));
         toTargetY = (Py(1)-KITTy(i));
         targetTangent = toTargetY / toTargetX;
-        if (abs(targetTangent - KITTtangent)<0.1)
+        if (abs(targetTangent - KITTtangent)<e) && (abs((KITTx(i)-Px(1)))<abs(KITTx(i-1)-Px(1)))
             break
         end
     end
 end
 plot(KITTx, KITTy)
-lineX = [KITTx(end), KITTx(end)+100];
-lineY = [KITTy(end), KITTy(end)+100*KITTtangent];
+lineX = [KITTx(end)-400, KITTx(end), KITTx(end)+400];
+lineY = [KITTy(end)-400*KITTtangent, KITTy(end), KITTy(end)+400*KITTtangent];
 plot(lineX, lineY)
 
 
-%TODO: shift turning radius to let it start at a certain starting position
-%and calculate with every iteration if the tangent gets close to the target
-%position. if it does, stop the loop and plot the line to the target.
+%TODO: decide whether to go right or left turn
 
