f26338187663ce45068f610b2037e60d033b2726
Sander Delfos <sdelfos94@gmail.com>
2018-05-25 22:12:05 +0200

    It can now plot a trajectory to a target. Does not work from all start positiions and orientations yet.

--- Matlab/Final/trajectory.m
+++ Matlab/Final/trajectory.m
@@ -1,3 +1,9 @@
+clear all
+KITTStartX = 50;
+KITTStartY = 50;
+KITTStartOrientation = 0;
+KITTStartOrientation = deg2rad(KITTStartOrientation);
+
 figure
 hold on
 xlim([-10 470])
@@ -11,14 +17,28 @@ Mx=[460 0 0 460 230];
 My=[0 0 460 460 460];
 scatter(Px,Py,'r','x')
 scatter(Mx,My,'b')
+scatter(KITTStartX,KITTStartY,'g')
 
 %plot turning radius
-r = 85.5;
+r = 87.5;
 for i=1:100
-    KITTx(i) = r*cos(0.1*i);
-    KITTy(i) = r*sin(0.1*i);
+    KITTx(i) = r*cos(-0.1*(i-1)+KITTStartOrientation+pi)+KITTStartX +r*cos(KITTStartOrientation);
+    KITTy(i) = r*sin(-0.1*(i-1)+KITTStartOrientation+pi)+KITTStartY +r*sin(KITTStartOrientation);
+    if (i>1)
+        KITTtangent = (KITTy(i)-KITTy(i-1))/(KITTx(i)-KITTx(i-1));
+        toTargetX = (Px(1)-KITTx(i));
+        toTargetY = (Py(1)-KITTy(i));
+        targetTangent = toTargetY / toTargetX;
+        if (abs(targetTangent - KITTtangent)<0.1)
+            break
+        end
+    end
 end
 plot(KITTx, KITTy)
+lineX = [KITTx(end), KITTx(end)+100];
+lineY = [KITTy(end), KITTy(end)+100*KITTtangent];
+plot(lineX, lineY)
+
 
 %TODO: shift turning radius to let it start at a certain starting position
 %and calculate with every iteration if the tangent gets close to the target
