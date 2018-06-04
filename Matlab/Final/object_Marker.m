0cf119cd9e90c7aee684b837276e898f1feaa2e4
emielvv <38717827+emielvv@users.noreply.github.com>
2018-06-04 20:29:49 +0200

    Add files via upload
    Returns coordinates of object

---
+++ Matlab/Final/object_Marker.m
@@ -0,0 +1,36 @@
+%Gives coördinates of object closer or equal to a distance of
+%'distance_bound'
+%Function needs the current x and y coordinates of the car, the distance
+%sensor values and the car_orientation
+%Returns the x and y coordinate of the object (returns only a single
+%coordinate!)
+function [x_object,y_object] = object_Marker(x_car,y_car,x_sensor_l, x_sensor_r,car_orientation)
+sensor_min = min(x_sensor_l, x_sensor_r);
+distance_bound = 10; 
+if sensor_min < distance_bound
+        %quadrant 1
+    if car_orientation <= 90
+        x_object = x_car + sensor_min * cos(car_orientation)
+        y_object = x_car + sensor_min * sin(car_orientation)
+        
+        %quadrant 2
+    elseif (car_orientation > 90) && (car_orientation <= 180)
+        x_object = x_car - sensor_min * cos(car_orientation)
+        y_object = x_car + sensor_min * sin(car_orientation)
+        
+        %quadrant 3
+    elseif (car_orientation > 180) && (car_orientation <= 270)
+        x_object = x_car - sensor_min * cos(car_orientation)
+        y_object = x_car - sensor_min * sin(car_orientation)
+        
+        %quadrant 4
+    elseif (car_orientation > 270) && (car_orientation <= 360)
+        x_object = x_car + sensor_min * cos(car_orientation)
+        y_object = x_car - sensor_min * sin(car_orientation)
+        
+    end
+end
+        
+
+end
+
