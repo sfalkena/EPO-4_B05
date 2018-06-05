6c384a49db9a1578622654533412d32260527868
emielvv <38717827+emielvv@users.noreply.github.com>
2018-06-05 12:27:22 +0200

    Update object_Marker.m

--- Matlab/Final/object_Marker.m
+++ Matlab/Final/object_Marker.m
@@ -5,21 +5,56 @@
 %Returns the x and y coordinate of the object (returns only a single
 %coordinate!)
 
-function [object,x_object,y_object] = object_Marker(x_car,y_car,x_sensor_l, x_sensor_r,car_orientation)
-sensor_min = min(x_sensor_l, x_sensor_r);
+
+function [object,x_object,y_object] = object_Marker(x_car,y_car,r_sensor_l, r_sensor_r,car_orientation)
+sensor_min = min(r_sensor_l, r_sensor_r)
+%difference between measurement left and right sensor
+diff_sensor = r_sensor_l - r_sensor_r
+
+%distance of middle between sensors to one of the sensors
+radius_compensation = 0.175;
+
+%left sensor location is perpendicular to the forward location
+angle_compensation_l = mod(car_orientation + 0.5 * pi, 2*pi);
+x_compensation_l = radius_compensation * cos(angle_compensation_l);
+y_compensation_l = radius_compensation * sin(angle_compensation_l);
+
+angle_compensation_r = mod(car_orientation - 0.5 * pi, 2* pi);
+x_compensation_r = radius_compensation * cos(angle_compensation_r);
+y_compensation_r = radius_compensation * sin(angle_compensation_r);
+
+R = sqrt(radius_compensation^2 + sensor_min^2)
 %distance_bound can be dependent of the distance to destination in the
 %future
-distance_bound = 10; %arbitrary value for testing
+distance_bound = 1; %arbitrary value for testing
 if sensor_min < distance_bound
-    object = 1;
+    object = 1
+
+        %left sensor gives 10cm higher than right sensor
+    if diff_sensor > 0.10
+        
+        x_object = x_car + sensor_min * cos(car_orientation) + x_compensation_r
+        y_object = x_car + sensor_min * sin(car_orientation) + y_compensation_r
+    elseif diff_sensor < -0.10
+        x_object = x_car + sensor_min * cos(car_orientation) + x_compensation_l
+        y_object = x_car + sensor_min * sin(car_orientation) + y_compensation_l
+    else diff_sensor = 0
         x_object = x_car + sensor_min * cos(car_orientation)
         y_object = x_car + sensor_min * sin(car_orientation)
-
+    end
+    
     else
+        if abs(diff_sensor) > (0.1*R)
     %no object
-    object = 0;
-    %default value for when no object is close enough
-x_object = -1;
-y_object = -1;
+    object = 0
+    x_object = -1;
+    y_object = -1;
+        else
+            object = 1;
+        x_object = x_car + sensor_min * cos(car_orientation)
+        y_object = x_car + sensor_min * sin(car_orientation)
+        diff_sensor
+        R
+end
 end
 end
