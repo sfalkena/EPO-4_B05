595c33457d994e5073181da712620f751647bab1
Ivor <bas.ivor@gmail.com>
2018-06-08 09:02:16 +0200

    3d localization

--- Matlab/Execute_TDOA.m
+++ Matlab/Execute_TDOA.m
@@ -7,25 +7,38 @@ a = RXXr(6,:,:);                    %Measurement at location a (for testing)
 b = RXXr(7,:,:);                    %Measurement at location b (for testing)
 c = RXXr(8,:,:);                    %Measurement at location c (for testing)
 m = RXXr(9,:,:);                    %Measurement at location m (for testing)
-y_ref = RXXr(2,:,2);                %Select recording at mic2 as reference
+
+load('audiodata_B5_2.mat');         %Load reference data
+y_ref = RXXr(2,:,5);                %Select recording at mic2 as reference
 
 %get_rec();                         %Record the audio signal and store it in RXXr 
 
-yr1 = a(:,:,1);                  %Recording at mic1                     
-yr2 = a(:,:,2);                  %Recording at mic2
-yr3 = a(:,:,3);                  %Recording at mic3
-yr4 = a(:,:,4);                  %Recording at mic4
-yr5 = a(:,:,5);                  %Recording at mic5 (not used)
+yr1 = m(:,:,1);                  %Recording at mic1                     
+yr2 = m(:,:,2);                  %Recording at mic2
+yr3 = m(:,:,3);                  %Recording at mic3
+yr4 = m(:,:,4);                  %Recording at mic4
+yr5 = m(:,:,5);                  %Recording at mic5 (not used)
 
-d1 = Calculate_TDOA(yr1,y_ref);     %Relative distance from mic1
-d2 = Calculate_TDOA(yr2,y_ref);     %Relative distance from mic2
-d3 = Calculate_TDOA(yr3,y_ref);     %Relative distance from mic3
-d4 = Calculate_TDOA(yr4,y_ref);     %Relative distance from mic4
+%Real distance from mics
+d1s = Calculate_TDOA(yr1,y_ref);     %Relative distance from mic1
+d2s = Calculate_TDOA(yr2,y_ref);     %Relative distance from mic2
+d3s = Calculate_TDOA(yr3,y_ref);     %Relative distance from mic3
+d4s = Calculate_TDOA(yr4,y_ref);     %Relative distance from mic4
+d5s = Calculate_TDOA(yr5,y_ref);     %Relative distance from mic5
 
-r12 = (d1-d2)/Fs *v;
-r13 = (d1-d3)/Fs *v;
-r14 = (d1-d4)/Fs *v;
-r23 = (d2-d3)/Fs *v;
-r24 = (d2-d4)/Fs *v;
-r34 = (d3-d4)/Fs *v;
-[x_cor,y_cor] = localization(r12,r13,r14,r23,r24,r34)  %Retrieve x and y coordinates
+
+
+%Relative distances
+r12 = (d1s-d2s)/Fs *v;
+r13 = (d1s-d3s)/Fs *v;
+r14 = (d1s-d4s)/Fs *v;
+r15 = (d1s-d5s)/Fs *v;
+r23 = (d2s-d3s)/Fs *v;
+r24 = (d2s-d4s)/Fs *v;
+r25 = (d2s-d5s)/Fs *v;
+r34 = (d3s-d4s)/Fs *v;
+r35 = (d3s-d5s)/Fs *v;
+r45 = (d4s-d5s)/Fs *v;
+
+
+[x_cor,y_cor,z_cor] = localization(r12,r13,r14,r15,r23,r24,r25,r34,r35,r45)  %Retrieve xyz coordinates of the car
