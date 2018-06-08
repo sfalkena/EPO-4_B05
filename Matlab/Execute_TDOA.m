89d1cfb9be52da5bb8588ccf965c1b7f2ee47021
Sieger Falkena <s.falkena@forze-delft.nl>
2018-06-08 11:20:10 +0200

    3d implementation

--- Matlab/Execute_TDOA.m
+++ Matlab/Execute_TDOA.m
@@ -5,7 +5,7 @@ a=6; b=7; c=8; m=9;                  %different measurements from B5_1
 load 'ref.mat';                     %Load reference data
 
 %create axes for plots
-freq_y = fftshift(fft(RXXr(t,:,1)));
+freq_y = fftshift(fft(RXXr(:,:,1)));
 freq_x = fftshift(fft(ref));
 N=length(freq_x);
 Omega = pi*[-1: 2/N : 1-1/N];
@@ -18,13 +18,13 @@ F_1=(Omega_1*Fs)/(2*pi);
 t_1=linspace(0,(N_1/Fs),N_1);
 t_1=transpose(t_1);
 
-t=a; %choose measurement point
+l=a; %choose measurement point
 %calculate impulse responses
-h1=ch2(ref,RXXr(t,:,1)).^2;
-h2=ch2(ref,RXXr(t,:,2)).^2;
-h3=ch2(ref,RXXr(t,:,3)).^2;
-h4=ch2(ref,RXXr(t,:,4)).^2;
-h5=ch2(ref,RXXr(t,:,5)).^2;
+h1=ch2(ref,RXXr(l,:,1)).^2;
+h2=ch2(ref,RXXr(l,:,2)).^2;
+h3=ch2(ref,RXXr(l,:,3)).^2;
+h4=ch2(ref,RXXr(l,:,4)).^2;
+h5=ch2(ref,RXXr(l,:,5)).^2;
 
 % figure
 % hold on
@@ -34,11 +34,15 @@ h5=ch2(ref,RXXr(t,:,5)).^2;
 % xlabel('Time(s)')
 % ylabel('Amplitude')
 
-r12 = TDOA(h1,h2,'r12');        %Relative distance from mic1
-r13 = TDOA(h1,h3,'r13');
-r14 = TDOA(h1,h4,'r14');
-r23 = TDOA(h2,h3,'r23');
-r24 = TDOA(h2,h4,'r24');
-r34 = TDOA(h3,h4,'r34');
+r12 = Calculate_TDOA(h1,h2);        %Relative distance from mic1
+r13 = Calculate_TDOA(h1,h3);
+r14 = Calculate_TDOA(h1,h4);
+r15 = Calculate_TDOA(h1,h5);
+r23 = Calculate_TDOA(h2,h3);
+r24 = Calculate_TDOA(h2,h4);
+r25 = Calculate_TDOA(h2,h5);
+r34 = Calculate_TDOA(h3,h4);
+r35 = Calculate_TDOA(h3,h5);
+r45 = Calculate_TDOA(h4,h5);
 
-[x_cor,y_cor] = localization(r12,r13,r14,r23,r24,r34)  %Retrieve x and y coordinates
+[x_cor,y_cor,z_cor] = localization(r12,r13,r14,r15,r23,r24,r25,r34,r35,r45)  %Retrieve x and y coordinates
--- Matlab/localization.m
+++ Matlab/localization.m
@@ -1,10 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Simulation of localisation (Appendix C)
-function [x_cor,y_cor] = localization(r12,r13,r14,r23,r24,r34)
+function [x_cor,y_cor,z_cor] = localization(r12,r13,r14,r15,r23,r24,r25,r34,r35,r45) 
 
-m = [460 0; 0 0; 0 460; 460 460]';          %locations of the mics
-for i = 1:4
-    mabs(i) = m(1,i)^2 + m(2,i)^2;
+mic_h = 50;
+mic5_h = 80;
+m = [460 0 mic_h; 0 0 mic_h; 0 460 mic_h; 460 460 mic_h; 230 460 mic5_h]';          %locations of the mics
+for i = 1:5
+    mabs(i) = m(1,i)^2 + m(2,i)^2 + m(3,i)^2;
 end
 
 %Generate matrix A
@@ -12,32 +14,52 @@ clear A;
 A(1,:) = 2*(m(:,2)-m(:,1))';
 A(2,:) = 2*(m(:,3)-m(:,1))';
 A(3,:) = 2*(m(:,4)-m(:,1))';
-A(4,:) = 2*(m(:,3)-m(:,2))';
-A(5,:) = 2*(m(:,4)-m(:,2))';
-A(6,:) = 2*(m(:,4)-m(:,3))';
-A(1,3) = -2*r12;
-A(2,4) = -2*r13;
-A(3,5) = -2*r14;
-A(4,4) = -2*r23;
-A(5,5) = -2*r24;
-A(6,5) = -2*r34;
+A(4,:) = 2*(m(:,5)-m(:,1))';
 
+A(5,:) = 2*(m(:,3)-m(:,2))';
+A(6,:) = 2*(m(:,4)-m(:,2))';
+A(7,:) = 2*(m(:,5)-m(:,2))';
+
+A(8,:) = 2*(m(:,4)-m(:,3))';
+A(9,:) = 2*(m(:,5)-m(:,3))';
+
+A(10,:) = 2*(m(:,5)-m(:,4))';
+
+A(1,4) = -2*r12;
+A(2,5) = -2*r13;
+A(3,6) = -2*r14;
+A(4,7) = -2*r15;
+
+A(5,5) = -2*r23;
+A(6,6) = -2*r24;
+A(7,7) = -2*r25;
+
+A(8,6) = -2*r34;
+A(9,7) = -2*r35;
+
+A(10,7) = -2*r45;
+
+ 
 %Generate matrix b
 clear b;
 b(1) = r12^2 - mabs(1) + mabs(2);
 b(2) = r13^2 - mabs(1) + mabs(3);
 b(3) = r14^2 - mabs(1) + mabs(4);
-b(4) = r23^2 - mabs(2) + mabs(3);
-b(5) = r24^2 - mabs(2) + mabs(4);
-b(6) = r34^2 - mabs(3) + mabs(4);
+b(4) = r15^2 - mabs(1) + mabs(5);
+
+b(5) = r23^2 - mabs(2) + mabs(3);
+b(6) = r24^2 - mabs(2) + mabs(4);
+b(7) = r25^2 - mabs(2) + mabs(5);
+
+b(8) = r34^2 - mabs(3) + mabs(4);
+b(9) = r35^2 - mabs(3) + mabs(5);
+
+b(10) = r45^2 - mabs(4) + mabs(5);
 b = b';
 
-A_T = transpose(A);
-C = A_T*A;
-C = inv(C);
-C = C*A_T;
-y = C*b;
+y=inv(A'*A)*A'*b;
 
 x_cor = y(1);                       %x_coordinate car
 y_cor = y(2);                       %y_coordinate car
+z_cor = y(3);                       %z_coordinate car
 end
