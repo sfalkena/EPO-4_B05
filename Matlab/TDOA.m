b82c949bb4378c6b4fe5ae990b346182c018df25
Sander Delfos <sdelfos94@gmail.com>
2018-06-09 23:31:14 +0200

    Fixed TDOA not choosing first peak

--- Matlab/TDOA.m
+++ Matlab/TDOA.m
@@ -15,11 +15,24 @@ plot(h22)
 
 M2=max(h22);
 F2=find(h22==M2);
+M22=max(h22(1:Ts-10))
+F22=find(h22==M22);
+if (2*M22 > M2)
+    F2 = F22;
+    M2 = M22;
+end
 scatter(F2(1),M2);
 
 M1=max(h11);
 F1=find(h11==M1);
+M11=max(h11(1:Ts-100));
+F11=find(h11==M11);
+if (2*M11 > M1)
+    F1 = F11;
+    M1 = M11;
+end
 scatter(F1,M1);
+
 
 legend(p(2),p(3),'F2','F1')
 Diff=F1-F2; %Aantal samples verschil
