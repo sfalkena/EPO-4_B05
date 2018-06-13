903c4cc15b0ca458df276d0cc6a15111cdc8a7e2
Sieger Falkena <s.falkena@forze-delft.nl>
2018-06-13 08:20:38 +0200

    cleanup of continuous audio branch

--- Matlab/Calculate_TDOA.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%
-function [ Npeak ] = Calculate_TDOA(y2,y1)
-Fs = 48000;                     %Sampling frequency
-v = 34300;                      %Speed of sound in air [cm/s]
-
-h = ch3(y2,y1);                 %Find channel response
-hnew = h(1:4999);               %Remove later peaks
-[~,Npeak] = max(hnew);          %Find location peak [samples]
-end
--- Matlab/Setupfile.m
+++
@@ -1,5 +0,0 @@
-setpref('dsp','portaudioHostApi',3);
-pause(2)
-pa_wavrecord(1,1,20*8000) ;
-% EPO4_audio_record('B5_1', 15000,7500,2500,'ebeb9a61',48e3,5,1,3);
-% EPO4_audio_record('B5_1', 15000,7500,2500,'ebeb9a6192340f0f',48e3,5,1,3);
