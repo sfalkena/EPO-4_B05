bc493c3a8d132e68e0d0fe287b1196fcd4fc09a7
Sieger Falkena <s.falkena@forze-delft.nl>
2018-06-13 09:16:02 +0200

    Remove simulink files

--- Matlab/Simulink/KITTVelocity.m
+++
@@ -1,8 +0,0 @@
-figure;
-plot(velocityOutput.time, velocityOutput.signals.values);
-title('Velocity of KITT')
-xlabel('Time [s]')
-ylabel('Velocity [m/s]')
-
-index = find(velocityOutput.signals.values>16);
-IntersectionTime = velocityOutput.time(index(1))
